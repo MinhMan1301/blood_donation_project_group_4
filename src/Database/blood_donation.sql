d7a040ed47497d8e4249bd3b5da87944cd27c9ce
MinhMan1301 <165623483+MinhMan1301@users.noreply.github.com>
2025-10-19 09:41:58 +0700

    database integrated

--- src/Database/blood_donation.sql
+++ src/Database/blood_donation.sql
@@ -26,3 +26,23 @@ CREATE TABLE IF NOT EXISTS Blood_Bank (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (assigned_doctor_id) REFERENCES Account(user_id) ON DELETE SET NULL
 ) ENGINE=InnoDB;
+
+-- BLOOD INVENTORY
+CREATE TABLE IF NOT EXISTS Blood_Inventory(
+    unit_id INT AUTO_INCREMENT PRIMARY KEY,
+    blood_type ENUM('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-') NOT NULL,
+    RH ENUM('+', '-') NOT NULL,
+    volume_Litter DECIMAL(10,2) NOT NULL,
+    donated_date DATE NOT NULL,
+    expired_date DATE NOT NULL,
+    status ENUM('available', 'used', 'expired') DEFAULT 'available',
+    CONSTRAINT chk_dates CHECK (expired_date > donated_date)
+) ENGINE=InnoDB;
+
+-- DONATION EVENT
+CREATE TABLE IF NOT EXISTS Donation_Event (
+    donation_id INT PRIMARY KEY,
+    Date_event DATE NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    volume_collected DECIMAL(10, 2) NOT NULL
+)ENGINE=InnoDB;
