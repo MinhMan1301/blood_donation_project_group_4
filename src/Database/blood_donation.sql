31d4bb855cd32adc8fcdef8fb6f03b78af882609
williamhunter186 <106892927+williamhunter186@users.noreply.github.com>
2025-10-20 10:23:10 +0700

    Update blood_donation.sql

--- src/Database/blood_donation.sql
+++ src/Database/blood_donation.sql
@@ -1,33 +1,23 @@
 -- DONATION TABLE
-CREATE TABLE IF NOT EXISTS Donation (
-    donation_id INT AUTO_INCREMENT PRIMARY KEY,
-    donor_id INT NOT NULL,
-    bank_id INT NOT NULL,
-    event_id INT NULL,
-    inventory_id INT NULL,
-    donation_date DATE NOT NULL,
-    volume_Litter DECIMAL(10,2) CHECK (volume_Litter > 0),
-    status ENUM('pending', 'approved', 'rejected', 'completed') DEFAULT 'pending',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (donor_id) REFERENCES Account(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (bank_id) REFERENCES Blood_Bank(bank_id) ON DELETE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES Donation_Event(donation_id) ON DELETE SET NULL,
-    FOREIGN KEY (inventory_id) REFERENCES Blood_Inventory(unit_id) ON DELETE SET NULL
-) ENGINE=InnoDB;
-
--- REQUEST TABLE
 CREATE TABLE IF NOT EXISTS Request (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
     patient_id INT NOT NULL,
     bank_id INT NOT NULL,
     blood_type ENUM('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-') NOT NULL,
-    RH ENUM('+', '-') NOT NULL,
-    volume_Litter DECIMAL(10,2) CHECK (volume_Litter > 0),
-    request_date DATE NOT NULL,
-    status ENUM('pending', 'approved', 'rejected', 'fulfilled', 'cancelled') DEFAULT 'pending',
+    quantity INT CHECK (quantity > 0), -- milliliters
+    status ENUM('Pending', 'Approved', 'Fulfilled', 'Rejected') DEFAULT 'Pending',
+    date_request DATE NOT NULL,
+    date_response DATETIME NULL,
     fulfilled_inventory_id INT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (patient_id) REFERENCES Account(user_id) ON DELETE CASCADE,
     FOREIGN KEY (bank_id) REFERENCES Blood_Bank(bank_id) ON DELETE CASCADE,
     FOREIGN KEY (fulfilled_inventory_id) REFERENCES Blood_Inventory(unit_id) ON DELETE SET NULL
+);
+
+-- REQUEST TABLE
+CREATE TABLE IF NOT EXISTS Donation_Event (
+    donation_id INT AUTO_INCREMENT PRIMARY KEY,
+    volume INT CHECK (volume > 0), -- ml
+    date DATE NOT NULL,
+    location VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
